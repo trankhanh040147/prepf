38b79acdd8706d36389efea18af077a9ffe7b009
trankhanh040147 <trankhanh040147@gmail.com>
2026-01-12 08:07:16 +0700

    feat: update migration

--- internal/db/migrations/20250120000000_add_mode_to_sessions.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE sessions ADD COLUMN mode TEXT;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE sessions DROP COLUMN mode;
--- +goose StatementEnd
